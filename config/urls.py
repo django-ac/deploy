fe9f4905bf3b2e0b8821d2ca456f0f833c02459d
lhy <dev@lhy.kr>
2024-03-13 19:20:12 +0900

    static settings

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,3 +9,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", index),
 ]
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
