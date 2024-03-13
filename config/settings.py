3bd26aab2f0c3283a90caa8a76ad7b2698494361
lhy <dev@lhy.kr>
2024-03-13 19:20:12 +0900

    static settings

--- config/settings.py
+++ config/settings.py
@@ -117,6 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "_static"
+
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "_media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
